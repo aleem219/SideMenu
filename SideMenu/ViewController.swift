5b8ce5d3ec79f4d812153ae5911c3b54ef1a4ec3
Your GitHub aleem219 <Your GitHub qazialeem21@gmail.com>
2026-09-10 14:06:31 +0530

    Updated Code

--- SideMenu/ViewController.swift
+++ SideMenu/ViewController.swift
@@ -8,16 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Oulets
     @IBOutlet weak var sidemenuView: UIView!
     @IBOutlet weak var sidemenuLeadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var tableView: UITableView!
     
+    // MARK: - Variables
     private var isMenuOpen = false
     let myArray = ["row 1", "row 2", "row 3"]
     
+    
+    // MARK: - Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTbalViewDelegates()
+    }
+    
+    // MARK: - Button's Action
     @IBAction func btnSidelMenu(_ sender: UIBarButtonItem) {
+        handleSideMenuToggle()
+    }
+}
+
+// MARK: - Extension for ViewController
+extension ViewController  {
+    func setupTbalViewDelegates() {
         tableView.dataSource = self
         tableView.delegate = self
+    }
+    
+    func handleSideMenuToggle() {
         isMenuOpen.toggle()
         sidemenuLeadingConstraint.constant = isMenuOpen ? 0 : -225
         tableView.showsVerticalScrollIndicator = false
@@ -27,20 +48,22 @@ class ViewController: UIViewController {
     }
 }
 
-
+// MARK: - Extension for UITableViewDataSource & UITableViewDelegate
 extension ViewController : UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return myArray.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = myArray[indexPath.row]
         cell.selectionStyle = .none
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
 }
+
+// MARK: - ends here
